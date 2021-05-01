2a0bae2ac5bc92c5361b4d063f6b19eb375d079d
Hallover <suno0624@gmail.com>
2021-05-01 04:42:39 +0200

    updated
    앞으로 만들 계획을 일단 테스트로 만들어 봤어요.
    입력받은 리스트가 투두리스트와 달리 텍스트 인풋이어야 한다는 점!!

--- dr_context.js
+++ dr_context.js
@@ -1,15 +1,29 @@
-const contextForm = document.querySelector(".dr-tbl-input-context"),
-  contextInput = contextForm.querySelector(".dr-write-input"),
+const contextForm = document.querySelector(".dr-contextForm"),
+  contextInput = contextForm.querySelector(".dr-tbl-input-context"),
   contextList = document.querySelector(".dr-show-contextList");
 
-const CONTEXT_LS = "contxt";
+const CXT_LS = "contxt";
 
 let contxt = [];
 
-function paintToDo(){
+function paintToDo() {}
 
+function handleSubmit(event) {
+  event.preventDefault(); // 정의 된 고유 동작을 작동하지 못하게 함. p, input, textarea 입력 안되게 함
+  const currentCxtValue = contextInput.value;
+  paintContext(currentCxtValue);
+  contextInput.value = "";
 }
 
+function loadContext() {
+  const loadedContexts = localStorage.getItem(CXT_LS);
+  if (loadContext !== null) {
+    const parsedCxt = JSON.parse(loadContext);
+    parsedCxt.forEach(function (cxt) {
+      paintContext(cxt.text);
+    });
+  }
+}
 
 function init() {
   loadContext();
